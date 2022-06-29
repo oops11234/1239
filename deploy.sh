9aeca22933746cfa44bbdaf0ce3c4c88d9528a78
rick <oops11234@gmail.com>
2022-06-29 16:52:29 +0800

    readme edit

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/oops11234/1239.git master:gh-pages
+git push -f https://github.com/oops11234/Vue-API-CSS-practice.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  publicPath: '/vue-API-CSS-practice/',
+  publicPath: '/Vue-API-CSS-practice/',
   transpileDependencies: true,
 });
